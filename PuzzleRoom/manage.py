0e1b99d07a91e33bea722c410b0e84ece026f0ee
B00147423 <B00147423@users.noreply.github.com>
2024-12-12 20:03:16 +0000

    Fix Docker deployment and ASGI

--- PuzzleRoom/manage.py
+++ PuzzleRoom/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PuzzleRoom.PuzzleRoom.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PuzzleRoom.settings')
 
     try:
         from django.core.management import execute_from_command_line
